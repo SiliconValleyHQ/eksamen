6117f340cb5b88aa84f27e3915b5b0b2ccb46530
mariiusw <marius.wetterlin@hotmail.com>
2016-11-29 18:14:17 +0100

    luket ut alt som ikke er sever relatert i TicTacToe.java

---
+++ src/TicTacToe.java
@@ -0,0 +1,89 @@
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.InetAddress;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Scanner;
+
+public class TicTacToe implements Runnable {
+
+    private String ip = "localhost";
+    private int port = 22222;
+    private Scanner scanner = new Scanner(System.in);
+    private Thread thread;
+
+    private Socket socket;
+    private DataOutputStream dos;
+    private DataInputStream dis;
+    private ServerSocket serverSocket;
+
+    private boolean circle = true;
+    private boolean accepted = false;
+
+    public TicTacToe() {
+        System.out.println("Please input the IP: ");
+        ip = scanner.nextLine();
+        System.out.println("Please input the port: ");
+        port = scanner.nextInt();
+        while (port < 1 || port > 65535) {
+            System.out.println("The port you entered was invalid, please input another port: ");
+            port = scanner.nextInt();
+        }
+
+        if (!connect()) initializeServer();
+
+        thread = new Thread(this, "TicTacToe");
+        thread.start();
+    }
+
+    public void run() {
+        while (true) {
+            if (!circle && !accepted) {
+                listenForServerRequest();
+            }
+        }
+    }
+
+    private void listenForServerRequest() {
+        Socket socket = null;
+        try {
+            socket = serverSocket.accept();
+            dos = new DataOutputStream(socket.getOutputStream());
+            dis = new DataInputStream(socket.getInputStream());
+            accepted = true;
+            System.out.println("CLIENT HAS REQUESTED TO JOIN, AND WE HAVE ACEPTED");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private boolean connect() {
+        try {
+            socket = new Socket(ip, port);
+            dos = new DataOutputStream(socket.getOutputStream());
+            dis = new DataInputStream(socket.getInputStream());
+            accepted = true;
+        } catch (IOException e) {
+            System.out.println("Unable to connect to the address: " + ip + ":" + port + " | Starting a server");
+            return false;
+        }
+        System.out.println("Successfully connected to the server.");
+        return true;
+    }
+
+    private void initializeServer() {
+        try {
+            serverSocket = new ServerSocket(port, 8, InetAddress.getByName(ip));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        circle = false;
+    }
+
+    @SuppressWarnings("unused")
+    public static void main(String[] args) {
+        TicTacToe ticTacToe = new TicTacToe();
+    }
+
+}
