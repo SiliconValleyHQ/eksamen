0a5bbcd75d7bb2cbbf7f7ecc0c516bfc9786fcba
Bror <brorbrurberg@me.com>
2016-11-30 12:59:53 +0100

    Åpner nå 1 vindu per instans, og ikke 2

--- src/gui/DamSpillVindu.java
+++ src/gui/DamSpillVindu.java
@@ -13,14 +13,8 @@ public class DamSpillVindu extends Canvas {
     private SpillData spillbrett;
 
     public DamSpillVindu() {
-        JFrame vindu = new JFrame();
-        vindu.setBackground(Color.black);
-        vindu.setFont(new Font("verdana", Font.BOLD, 16));
-        vindu.setSize(400,400);
-        vindu.setVisible(true);
 
         spillbrett = new SpillData();
-
         SpillData nyttspill = new SpillData();
         nyttspill.startNyttSpill("Start nytt spill");
 
