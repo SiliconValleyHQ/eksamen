6c6934c91dc81427f650948e79f0449e74cbc2d7
Bror <brorbrurberg@me.com>
2016-12-01 09:45:16 +0100

    Videreutvikling av GUI i pakken spillLogikk

--- src/gui/DamSpillVindu.java
+++ src/gui/DamSpillVindu.java
@@ -1,22 +1,20 @@
 package gui;
 
-import javax.swing.*;
 import java.awt.*;
 import spillLogikk.*;
-
 
 /**
  * Created by Bror on 30.11.2016.
  */
 public class DamSpillVindu extends Canvas {
 
-    private SpillData spillbrett;
+    SpillData spillbrett;
 
     public DamSpillVindu() {
 
         spillbrett = new SpillData();
         SpillData nyttspill = new SpillData();
-        nyttspill.startNyttSpill("Start nytt spill");
+        nyttspill.startNyttSpill();
 
     }
 
--- src/spillLogikk/SpillRegler.java
+++ src/spillLogikk/SpillRegler.java
@@ -1,11 +1,25 @@
 package spillLogikk;
 
+import static java.awt.Color.*;
+
 /**
  * Created by Bror on 30.11.2016.
  */
 public class SpillRegler {
 
-    public void settOppBrikkerForNyttSpill() {
+    public void settOppNyttSpill() {
+
+        int[][] spillBrett = new SpillData().spillBrett;
+
+        for (int rad = 0; rad < 8; rad++) {
+            for (int kolonne = 0; kolonne < 8; kolonne++) {
+                if (rad % 2 == kolonne % 2) {
+                    if (rad < 3) {
+                        //spillBrett[rad][kolonne] = BLACK;
+                    }
+                }
+            }
+        }
     }
 
 }
