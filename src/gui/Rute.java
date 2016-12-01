4bd918074a2729d4258cc13d2be6f2b6ef5fc926
Kristian Hov Bjerke <kristianhb87@gmail.com>
2016-12-01 15:21:05 +0100

    masse testing

---
+++ src/gui/Rute.java
@@ -0,0 +1,54 @@
+package gui;
+
+/**
+ * Created by Weeto on 01/12/2016.
+ */
+    import java.awt.Color;
+    import java.awt.event.MouseEvent;
+    import java.awt.event.MouseListener;
+
+    import javax.swing.JPanel;
+
+    public class Rute extends JPanel implements MouseListener {
+
+        private int position;
+        final String[] kolonnebokstaver = { "A", "B", "C", "D", "E", "F", "G", "H"};
+
+        public Rute(int position) {
+            this.position = position;
+            setBackground(calculateColor(position));
+            addMouseListener(this);
+        }
+        private Color calculateColor(int p) {
+            boolean annenHverRute = (p % 2 == 0);
+            boolean annenHverRad = (p / 8) % 2 ==0;
+            return annenHverRute != annenHverRad ? Color.black : Color.white;
+        }
+        @Override
+        public void mouseClicked(MouseEvent e) {
+            String bokstav = kolonnebokstaver[position % 8];
+            System.out.println(position + " " + bokstav + ((position / 8)+1));
+        }
+        @Override
+        public void mousePressed(MouseEvent e) {
+            // TODO Auto-generated method stub
+
+        }
+        @Override
+        public void mouseReleased(MouseEvent e) {
+            // TODO Auto-generated method stub
+
+        }
+        @Override
+        public void mouseEntered(MouseEvent e) {
+            // TODO Auto-generated method stub
+
+        }
+        @Override
+        public void mouseExited(MouseEvent e) {
+            // TODO Auto-generated method stub
+
+        }
+
+    }
+
