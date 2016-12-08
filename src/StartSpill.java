101c001177c68e860aac5bf2c8769694fb867de4
mariiusw <marius.wetterlin@hotmail.com>
2016-12-08 11:49:07 +0100

    Funker dette?

---
+++ src/StartSpill.java
@@ -0,0 +1,9 @@
+import oppkobling.Server;
+
+public class StartSpill {
+
+    public static void main(String[] args) throws Exception {
+        new Server();
+    }
+
+}
---
+++ src/gui/DamSpill.java
@@ -0,0 +1,16 @@
+package gui;
+
+import spillLogikk.*;
+
+/**
+ * Created by Bror on 30.11.2016.
+ */
+public class DamSpill {
+
+    public DamSpill() {
+
+        new SpillRegler();
+
+    }
+
+}
