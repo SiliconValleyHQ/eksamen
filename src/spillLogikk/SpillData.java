02f3ecb06fe4176146fd0a8d819df25e1b8e45cd
mariiusw <marius.wetterlin@hotmail.com>
2016-12-09 10:36:13 +0100

    tester meg frem

--- src/spillLogikk/SpillData.java
+++ src/spillLogikk/SpillData.java
@@ -5,30 +5,30 @@ package spillLogikk;
  */
 public class SpillData {
 
-    private static final int rodSpiller = 1;
-    private static final int svartSpiller = 2;
-    public int[][] spillBrett;
-    private int aktivSpiller;
+	private static final int rodSpiller = 1;
+	private static final int svartSpiller = 2;
+	public int[][] spillBrett;
+	private int aktivSpiller;
 
-    /*
-    * Ikke fullstending, så gir feilmeldinger.. Ikke farlig
-    */
-    public SpillData() {
-        spillBrett = new int[8][8];
-        startNyttSpill();
-    }
+	/*
+	* Ikke fullstending, så gir feilmeldinger.. Ikke farlig
+	*/
+	public SpillData() {
+		spillBrett = new int[8][8];
+		startNyttSpill();
+	}
 
-    /*
-    * Ikke fullstending, så gir feilmeldinger.. Ikke farlig
-    */
-    public void startNyttSpill() {
+	/*
+	* Ikke fullstending, så gir feilmeldinger.. Ikke farlig
+	*/
+	public void startNyttSpill() {
 
-        SpillRegler settOppNyttSpill = new SpillRegler();
-        //settOppNyttSpill.settOppNyttSpill();
+		SpillRegler settOppNyttSpill = new SpillRegler();
+		//settOppNyttSpill.settOppNyttSpill();
 
-        aktivSpiller = rodSpiller;
-        SpillRegler alleLovligeTrekk = new SpillRegler();
-        //muligeTrekk = alleLovligeTrekk(aktivSpiller);
-    }
+		aktivSpiller = rodSpiller;
+		SpillRegler alleLovligeTrekk = new SpillRegler();
+		//muligeTrekk = alleLovligeTrekk(aktivSpiller);
+	}
 
 }
